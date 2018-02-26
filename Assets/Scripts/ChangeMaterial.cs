5ed3b1b945220d3afe05f6973ba5cc726a307f5d
Eduardo C. Flores Ambrosio <efloresambrosio@gmail.com>
2018-02-26 01:36:16 -0600

    First config added
    First config added

---
+++ Assets/Scripts/ChangeMaterial.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterial : MonoBehaviour {
+
+	void FixedUpdate()
+	{
+		//Hacer un raycast de vr
+		//Si esta colicionando con el cubo, usar un tag
+		//Cambiar el material del cubo mientras se este golpenado y el material no sea el que se requiere
+		//Cuando este saliendo el hit regresar la textura original
+	}
+}
---
+++ Assets/Scripts/MouseControll.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControll : MonoBehaviour
+{
+    public float mouseSensitivity = 100.0f;
+    public float clampAngle = 80.0f;
+
+    private float rotY = 0.0f;
+    private float rotX = 0.0f;
+
+    void Start()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
+
+        rotY += mouseX * mouseSensitivity * Time.deltaTime;
+        rotX += mouseY * mouseSensitivity * Time.deltaTime;
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+    }
+
+    public void CastARay()
+    {
+        //todo CASTEAR EL RAYO PARA VER EN DONDE SE GOLPEA O NO UN OBJETO
+    }
+}
